c54409399a4255a6406a5075f6503b0dca79d63c
Rik Smeets <30838294+rik-smeets@users.noreply.github.com>
2024-12-10 16:36:49 +0100

    Using special characters in Reqnroll

---
+++ SpecialChars/SpecialCharacters.feature
@@ -0,0 +1,5 @@
+Feature: Special characters
+
+Scenario: Scenario
+	Given these special characters in a step argument: '€ è •'
+	And these special characters € è • simply in a step
---
+++ SpecialChars/Steps.cs
@@ -0,0 +1,19 @@
+﻿using Reqnroll;
+
+namespace SpecialChars;
+
+[Binding]
+public class Steps
+{
+	[Given("these special characters in a step argument: {string}")]
+	public void GivenTheseSpecialCharactersInAStepArgument(string specialCharacters)
+	{
+		Console.WriteLine(specialCharacters); // all special characters replaced by � (replacement character)
+	}
+
+	[Given("these special characters € è • simply in a step")]
+	public void GivenTheseSpecialCharactersESimplyInAStep()
+	{
+		throw new InvalidOperationException("This won't be hit, since Reqnroll doesn't recognize the binding");
+	}
+}
